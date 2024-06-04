e1cc2d3d7a6efa4a88521f1f47ab3c7568f6a650
Lorenzo Venerandi <lollo.venerandi@gmail.com>
2024-06-05 00:57:23 +0200

    alarm logic

---
+++ Project/data-proxy-server/apps/custom/alarm.py
@@ -0,0 +1,58 @@
+import datetime
+from flask import url_for
+import yaml
+import requests
+
+class AlarmScheduler():
+
+    def __init__(self):
+        super().__init__()
+    
+    def load_alarms(self, alarms_path = './alarms.yaml'):
+        # load alarms from file
+        self._alarms_path = alarms_path
+        with open(self._alarms_path, 'r') as file:
+            alarms = yaml.load(file, Loader=yaml.FullLoader)
+            self._alarms = alarms
+
+    def get_alarms(self):
+        return self._alarms['devices']
+    
+    def add_alarm(self, device_id, alarm):
+        self._alarms[device_id].append(alarm)
+        self.save_alarms()
+    
+    def remove_alarm(self, device_id, alarm):
+        self._alarms[device_id].remove(alarm)
+        self.save_alarms()
+
+    def save_alarms(self):
+        with open(self._alarms_path, 'w') as file:
+            yaml.dump(self._alarms, file)
+
+    def trigger_alarm(self, device_id, alarm):
+        print(f'Alarm triggered for device {device_id}: {alarm}')
+
+        # call the device to trigger the alarm with post request
+        requests.post(url_for('home_blueprint.trigger_alarm'), data={device_id: alarm})
+
+
+    def check_alarms(self):
+        
+        now = datetime.datetime.now()
+
+        for entry in self._alarms['devices']:
+            for alarm in entry['alarms']:
+                
+                alarm_time = datetime.datetime.strptime(alarm['time'], '%H:%M').time()
+
+                if 'date' in alarm:
+                    alarm_date = datetime.datetime.strptime(alarm['date'], '%Y-%m-%d').date()
+                    if now.date() == alarm_date and now.time() == alarm_time:
+                        self.trigger_alarm(entry['device_id'], alarm['time'])
+                        continue
+
+                if 'days' in alarm:
+                    if now.weekday() in alarm['days'] and now.time() == alarm_time:
+                        self.trigger_alarm(entry['device_id'], alarm)
+                        continue
