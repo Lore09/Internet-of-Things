fb803a2938d2af9fcb5c41013e3e048ccb0645e9
luca <Luca3700>
2024-06-07 21:54:23 +0200

    add loop to check resleep

--- Project/data-analysis-server/analyze/check_resleep.py
+++ Project/data-analysis-server/analyze/check_resleep.py
@@ -1,7 +1,11 @@
+import time
 from datetime import datetime
 import pandas as pd
 from analyze.read_from_db import read_data_with_time_period
 from analyze.analyze_sleep import detect_sleep_periods
+
+import threading
+import requests
 
 
 def get_resleep_minutes(InfluxDB, names, pillow_weight, head_weight, how_many_minutes=5):
@@ -15,3 +19,30 @@ def get_resleep_minutes(InfluxDB, names, pillow_weight, head_weight, how_many_mi
                                                                 min_sleep_duration_minutes=1, time_unit_hour=False)
 
     return sleep_duration
+
+
+def repeat_until_woke_up(InfluxDB, names, pillow_weight, head_weight, device_id, alarm, how_many_minutes=5):
+    woke_up = False
+    
+    while not woke_up:
+        time.sleep(how_many_minutes * 60)
+        sleep_duration = get_resleep_minutes(InfluxDB, names, pillow_weight, head_weight, \
+                                            how_many_minutes=how_many_minutes)
+        if sleep_duration < 1.0:
+            """
+            if the person sleeps doesn't sleep at least one minute
+            in the last 5, then we consider it as woke up
+            """
+            woke_up = True
+        else:
+            # otherwise trigger alarm again
+            requests.post("http://127.0.0.1:5000/api/trigger_alarm", data={device_id: alarm})
+         
+
+
+def create_thread_until_woke_up(InfluxDB, names, pillow_weight, head_weight, device_id, alarm, how_many_minutes=5):
+    # Create a thread to run the repeated_execution function with parameters
+    execution_thread = threading.Thread(target=repeat_until_woke_up, args=(InfluxDB, names, pillow_weight, \
+                                                                            head_weight, device_id, alarm))
+    # Start the thread
+    execution_thread.start()
--- Project/data-analysis-server/app/main.py
+++ Project/data-analysis-server/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from analyze.analyze_sleep import compute_sleep_time_for_each_day
 from analyze.analyze_sleep import compute_sleep_time_for_remaining_days
-from analyze.check_resleep import get_resleep_minutes
+from analyze.check_resleep import get_resleep_minutes, create_thread_until_woke_up
 from app.__init__ import create_influxDB_client, define_names
 
 
@@ -13,7 +13,7 @@ pillow_weight   = 4  # Example weight of the pillow
 head_weight     = 10    # Example weight of the head
 
 # compute the sleep time for each day
-hours_of_sleep_per_day = compute_sleep_time_for_each_day(InfluxDB, names, pillow_weight, head_weight)
+# hours_of_sleep_per_day = compute_sleep_time_for_each_day(InfluxDB, names, pillow_weight, head_weight)
 
 
 @app.get("/")
@@ -28,12 +28,20 @@ async def get_sleep_time_for_each_day():
     return new_hours_of_sleep_per_day
 
 
-@app.get("/analyze/check_resleep")
-async def check_resleep():
-    sleep_duration = get_resleep_minutes(InfluxDB, names, pillow_weight, head_weight)
+# @app.get("/analyze/check_resleep")
+# async def check_resleep():
+#     sleep_duration = get_resleep_minutes(InfluxDB, names, pillow_weight, head_weight)
     
-    if sleep_duration >= 1:
-        # trigger again the alarm
-        return {"resleep": 1}
-    else:
-        return {"resleep": 0}
+#     if sleep_duration >= 1:
+#         # trigger again the alarm
+#         return {"resleep": 1}
+#     else:
+#         return {"resleep": 0}
+
+@app.get("/analyze/check_resleep")
+async def check_resleep(device_id, alarm):
+    """
+    function that create a thread that check 
+    every 5 minutes if the person woke up
+    """
+    create_thread_until_woke_up(InfluxDB, names, pillow_weight, head_weight, device_id, alarm)
