c085a4b54745b6ce1ce88a0eade76fc429bb26a3
luca <Luca3700>
2024-06-15 13:28:30 +0200

    setup_device.py update comment

--- Project/data-analysis-server/analyze/setup_device.py
+++ Project/data-analysis-server/analyze/setup_device.py
@@ -4,20 +4,22 @@ import requests
 from analyze.read_from_db import read_data_with_time_period
 
 """
-setup of the device, 
-to obtain the weight of the pillow and the weight of the head.
-To signal the start of the setup process we play the tone,
-then for the successive 20 seconds we will record the values of the sensor,
-we stop the reproduction of the sound and 
-then we will analyze them to obtain the weight of the pillow.
-We wait some seconds and then we play the tone again.
-We will repeat again the procedure a second time but with the person on the bed 
-to obtain the weight of the head.
+SENSOR DATA CALIBRATION
+Done in order to obtain the value recorded by the pressure sensor when the user is and isn't on the bed.
+- the sampling rate is augmented
+- the alarm sounds for five seconds to signal the user to lay down on the bed
+- the micro-controller sends data to the data proxy server related to the values reported by the pressure sensor when the user is on the bed
+- the data analysis server computes the average of these values and save it
+- the alarm sounds for five seconds to signal the user to get up from the bed
+- the micro-controller sends data to the data proxy server related to the values reported by the pressure sensor when the user is not on the bed
+- the data analysis server computes the average of these values and save it
+- the sampling rate is reset to the default value
 """
 
 headers = {
     'Content-Type': 'application/x-www-form-urlencoded'
 }
+
 
 def compute_weight(InfluxDB, names, device_id):
 
@@ -37,8 +39,6 @@ def compute_weight(InfluxDB, names, device_id):
     # compute weight
     weight = sum(df[names.df_pressure_value])/len(df)
     return weight
-
-
 
 
 def setup_device(InfluxDB, names, device_id):
